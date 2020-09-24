ca9c68a4173e7719eb8a3dd285568fef72255e90
TetsuDoragon <37674166+TetsuDoragon@users.noreply.github.com>
2020-09-23 23:12:40 -0300

    Version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## [0.0.1] - 23/09/2020.
+
+* Create Styles - DEFAULT, BORDERLESS, NODE, NODE_BORDERLESS
+* Fix some bugs.
+
 ## [0.0.1] - 22/09/2020.
 
 * First release.
---
+++ lib/image_slider_style.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'image_slider_options.dart';
+
+enum ImageSliderStyleEnum {
+  DEFAULT,
+  BORDERLESS,
+  NODE,
+  NODE_BORDERLESS,
+}
+
+class ImageSliderStyleOptions {
+  final ImageSliderStyleEnum style;
+  final ImageSliderStyle options;
+  final Color color;
+  final Color borderColor;
+  final double width;
+  final double imageWidth;
+
+  ImageSliderStyleOptions({
+    this.style = ImageSliderStyleEnum.DEFAULT,
+    this.color = Colors.grey,
+    this.width = 200,
+    this.imageWidth = 40,
+    this.borderColor = Colors.white,
+  }) : this.options = ImageSliderStyle(style, imageWidth);
+}
